e5bb202920c7a33f459a8a1ffb0a3f31fa2791b1
AlenaSmile <alenaeliseenko2003@gmail.com>
2024-06-19 19:53:30 +0300

    .gitignore package-lock.json package.json public/index.html public/logo192.png public/logo512.png public/manifest.json public/robots.txt src/App.css src/App.js src/App.test.js src/components/CurrencyConverter.js src/components/ExchangeRates.js src/components/changeStyle.css src/index.css src/index.js src/logo.svg src/pages/ConverterPage.js src/pages/RatesPage.js src/reportWebVitals.js src/setupTests.js

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import RatesPage from "./pages/RatesPage";
+import ConverterPage from "./pages/ConverterPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const App = () => {
+    return ( <
+        Router >
+        <
+        Navbar bg = "dark"
+        variant = "dark"
+        expand = "lg" >
+        <
+        Container >
+        <
+        Navbar.Brand href = "/" > Currency App < /Navbar.Brand> <
+        Navbar.Toggle aria - controls = "basic-navbar-nav" / >
+        <
+        Navbar.Collapse id = "basic-navbar-nav" >
+        <
+        Nav className = "me-auto" >
+        <
+        Nav.Link as = { Link }
+        to = "/" > Converter < /Nav.Link> <
+        Nav.Link as = { Link }
+        to = "/rates" > Exchange rate < /Nav.Link> <
+        /Nav> <
+        /Navbar.Collapse> <
+        /Container> <
+        /Navbar> <
+        Routes >
+        <
+        Route path = "/"
+        element = { < ConverterPage / > } > < /Route> <
+        Route path = "/rates"
+        element = { < RatesPage / > } > < /Route> <
+        /Routes> <
+        /Router>
+    )
+};
+
+export default App;
---
+++ src/pages/ConverterPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import CurrencyConverter from '../components/CurrencyConverter';
+
+const ConverterPage = () => {
+    return ( <
+        div >
+        <
+        CurrencyConverter / >
+        <
+        /div>
+    );
+};
+
+export default ConverterPage;
